176818f5e942181def4f95b42d5d9b60d36719ee
Avision Ho <avision.ho@education.gov.uk>
2019-08-28 07:13:52 +0100

    feat: add choropleth map to analysis.
    This map visualises the points where casualties take place in Camden.

--- scripts/3_1_analysis_where.R
+++ scripts/3_1_analysis_where.R
@@ -19,12 +19,48 @@ library(dplyr)
 library(rgdal)
 library(ggplot2)
 
-shape_camden <- readOGR(dsn = "data/camden_ward_boundary/geo_export_10000172-4451-416d-89b7-a10ff0869756.shp")
+shape_camden <- readOGR(dsn = "data/camden_ward_boundary/geo_export_43237533-e0f3-4e84-9c11-3231b9f45a97.shp")
+# convert to data frame
 shape_camden_data <- fortify(model = shape_camden)
 
-map <- ggplot(data = shape_camden_data, aes(x = long, y = lat, group = group))
+
+# Prepare Raw Plots -------------------------------------------------------
+map <- ggplot(data = shape_camden_data, mapping = aes(x = long, y = lat, group = group))
 
 map + 
   geom_polygon(mapping = aes(fill = id)) +
   coord_fixed(1.3) +
   guides(fill = FALSE)
+
+
+# Prepare Data for Chloropleth --------------------------------------------
+centroids <- setNames(
+  do.call("rbind.data.frame", 
+          by(shape_camden_data, shape_camden_data$group, function(x) {Polygon(x[c('long', 'lat')])@labpt}))
+  , c('long', 'lat')
+) 
+
+centroids$label <- shape_camden_data$id[match(rownames(centroids), shape_camden_data$group)]
+
+theme_bare <- theme(
+  axis.line = element_blank(), 
+  axis.text.x = element_blank(), 
+  axis.text.y = element_blank(),
+  axis.ticks = element_blank(), 
+  axis.title.x = element_blank(), 
+  axis.title.y = element_blank(),
+  legend.text=element_text(size=7),
+  legend.title=element_text(size=8),
+  panel.background = element_blank(),
+  panel.border = element_rect(colour = "gray", fill=NA, size=0.5)
+)
+
+ggplot() + 
+  geom_polygon(data = shape_camden_data, mapping = aes(x = long, y = lat, group = group, fill = id)) +
+  geom_point(data = data_temp, mapping = aes(x = Longitude, y = Latitude)) +
+  coord_fixed(1.3) +
+  guides(fill = FALSE) +
+  # tried to add text of location but doesn't exist
+  #with(centroids, annotate(geom="text", x = long, y = lat, label=label, size=2)) +
+  theme_bare
+
